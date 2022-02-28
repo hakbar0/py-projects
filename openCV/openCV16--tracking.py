34187b9e8e6d3b7167c66476f32022b944410d40
Hakbar0 <haseeb2000@hotmail.com>
2022-02-28 22:41:01 +0000

    created foreground mask

--- openCV/openCV16--tracking.py
+++ openCV/openCV16--tracking.py
@@ -14,7 +14,7 @@ cv2.createTrackbar('hueHigher', 'Trackbars', 100, 179, nothing)
 cv2.createTrackbar('satLow', 'Trackbars', 100, 255, nothing)
 cv2.createTrackbar('satHigh', 'Trackbars', 255, 255, nothing)
 cv2.createTrackbar('valLow', 'Trackbars', 100, 255, nothing)
-cv2.createTrackbar('valHigh', 'Trackbars', 100, 255, nothing)
+cv2.createTrackbar('valHigh', 'Trackbars', 255, 255, nothing)
 
 ## want to keep this aspect ratio
 ## display width/height
@@ -40,6 +40,31 @@ while True:
     ## Grabbing a frame and then showing the frame
     cv2.imshow('piCam', frame)
     cv2.moveWindow('piCam', 0, 0)
+
+    ## Hard to track RGB, so convert it
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    hueLow = cv2.getTrackbarPos('hueLower', 'Trackbars')
+    hueUp = cv2.getTrackbarPos('hueHigher', 'Trackbars')
+
+    Ls = cv2.getTrackbarPos('satLow', 'Trackbars')
+    Us = cv2.getTrackbarPos('satHigh', 'Trackbars')
+
+    Lv = cv2.getTrackbarPos('valLow', 'Trackbars')
+    Uv = cv2.getTrackbarPos('valHigh', 'Trackbars')
+
+    ## lower bound
+    l_b = np.array([hueLow, Ls, Lv])
+    u_b = np.array([hueUp, Us, Uv])
+
+    ## Only want the smarties
+    FGmask = cv2.inRange(hsv, l_b, u_b)
+    cv2.imshow('FGmask', FGmask)
+    cv2.moveWindow('FGmask', 0, 410)
+
+    FG = cv2.bitwise_and(frame,frame, mask=FGmask)
+    cv2.imshow('FG', FG)
+    cv2.moveWindow('FG', 480, 0)
     ## checks every ms to see if key is pressed
     if cv2.waitKey(1) ==ord('q'):
         break
