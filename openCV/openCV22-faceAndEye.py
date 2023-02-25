17d835bfe54ad52e9817a03a165bc33b3c5c4417
Hakbar0 <haseeb2000@hotmail.com>
2023-02-25 21:03:36 +0000

    revert face box

--- openCV/openCV22-faceAndEye.py
+++ openCV/openCV22-faceAndEye.py
@@ -32,11 +32,11 @@ while True:
     ret, frame=cam.read()
     ##convert to grey as less computatial power
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
-    
+
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
     for (x,y,w,h) in faces:
-        cv2.rectangle(frame, (x,y), (x+w, y+y), (0,0,255), 2)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,0,255), 2)
 
     cv2.imshow('piCam', frame)
     cv2.moveWindow('piCam', 0, 0)
