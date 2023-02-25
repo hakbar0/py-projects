a91390255e255b287a4f75a193997b2feec7cd43
Hakbar0 <haseeb2000@hotmail.com>
2023-02-25 22:40:23 +0000

    init motors

--- openCV/openCV23-facePanTilt.py
+++ openCV/openCV23-facePanTilt.py
@@ -1,6 +1,32 @@
 import cv2
 print(cv2.__version__)
 
+import Adafruit_PCA9685
+import time
+
+## init servo motor
+
+## init the pca9685 using desired address/or bus
+pwm = Adafruit_PCA9685.PCA9685(address = 0x40, busnum = 1)
+
+down_min = 150 # min pulse length
+down_max = 600 # max pulse length
+down_mid = 300 # init pos of camera
+
+
+up_min = 450 # min pulse length
+up_max = 600 # max pulse length
+up_mid = 500 # init pos of camera
+
+pan = down_mid
+tilt = up_mid
+
+## set frequency
+pwm.set_pwm_freq(60)
+
+pwm.set_pwm(1,0,up_mid)
+pwm.set_pwm(0,0,down_mid)
+
 ## want to keep this aspect ratio
 ## display width/height
 dispW=640
